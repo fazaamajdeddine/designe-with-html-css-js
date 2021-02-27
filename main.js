ddc950584953749a4f24f08ff4b41fc727cc5f91
Fazaa Majdeddine <79419308+fazaamajdeddine@users.noreply.github.com>
2021-02-28 00:55:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const slider1 = document.getElementById('slide');
+const slider2 = document.getElementById('slidi');
+const buton = document.getElementById('btng');
+
+
+window.addEventListener('load' , majd );
+
+function majd(){
+
+slider1.classList.add('slide')
+slider1.style.width= '20%'
+slider2.classList.add('slidi')
+slider2.style.width = '80%'
+
+
+
+
+
+}
